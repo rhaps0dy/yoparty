c9f42927bd66adaa5142431b72d798b1879a2383
root <root@monkingme.com>
2015-02-28 16:22:25 -0500

    Server settings

--- yoparty_server/settings.py
+++ yoparty_server/settings.py
@@ -11,7 +11,17 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+STATIC_ROOT = '/opt/monkingme-django/static'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'yoparty',
+        'USER': 'django',
+        'PASSWORD': 'monkingme',
+        'HOST': ''
+    }
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -56,13 +66,6 @@ WSGI_APPLICATION = 'yoparty_server.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
