01c9013e641abbcac6490df33a04550b43457b78
Adrià <adria.garriga@gmail.com>
2015-03-01 06:51:40 +0100

    Tried deduplicating command

--- yoparty/static/yoparty/config.js
+++ yoparty/static/yoparty/config.js
@@ -1,20 +1,20 @@
 window.onload = function() {
     document.getElementById("mean").onclick = function(event) {
-        document.getElementById("command").value = "mean";
+        document.getElementById("command-id").value = "mean";
         document.getElementById("form").submit();
     };
 
     document.getElementById("userLoc").onclick = function(event) {
-        document.getElementById("command").value = "userLoc";
+        document.getElementById("command-id").value = "userLoc";
         document.getElementById("form").submit();
     };
 
     document.getElementById("userMean").onclick = function(event) {
-        document.getElementById("command").value = "userMean";
+        document.getElementById("command-id").value = "userMean";
         document.getElementById("form").submit();
     };
     document.getElementById("deleteUser").onclick = function(event) {
-        document.getElementById("command").value = "deleteUser";
+        document.getElementById("command-id").value = "deleteUser";
         document.getElementById("form").submit();
     };
 };
